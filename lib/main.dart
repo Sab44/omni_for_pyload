c39447e12f392750ba4d3ecf630e853857995ea7
Sab44 <64696149+sab44@users.noreply.github.com>
2025-12-08 13:48:38 +0100

    implement Theme setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:omni_for_pyload/core/service_locator.dart';
+import 'package:omni_for_pyload/domain/repositories/i_settings_repository.dart';
 import 'features/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupServiceLocator();
-  runApp(const App());
+
+  // Load settings before starting the app
+  final settingsRepository = getIt<ISettingsRepository>();
+  final settings = await settingsRepository.loadSettings();
+
+  runApp(App(initialSettings: settings));
 }
